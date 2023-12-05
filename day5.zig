46f454674f032f26e0d1c33f12d04c606ea0f0a2
Lauri Gustafsson <me@gustafla.space>
2023-12-05 21:15:31 +0200

    Add day 5 solution parsing code

--- day5.zig
+++ day5.zig
@@ -1,25 +1,31 @@
 const std = @import("std");
 
-const Error = std.fmt.ParseIntError;
+const List = std.ArrayList;
+const Gpa = std.heap.GeneralPurposeAllocator(.{});
+const Allocator = std.mem.Allocator;
+const Error = Gpa.Error || std.fmt.ParseIntError;
 
-const T = u32;
-const maxlen = 20;
-const sentinel = 0xffffffff;
-const List = [maxlen:sentinel]T;
+fn parseList(alloc: Allocator, slice: []const u8) Error!List(u32) {
+    var list = try List(u32).initCapacity(alloc, 20);
+    errdefer list.deinit();
 
-fn parseList(slice: []const u8) Error!List {
-    var list: List = std.mem.zeroInit(List, .{});
     var it = std.mem.splitScalar(u8, slice, ' ');
     while (it.next()) |str| {
         if (str.len == 0) {
             continue;
         }
         const integer = try std.fmt.parseInt(u32, str, 10);
-        list[list.len] = integer;
+        try list.append(integer);
     }
 
     return list;
 }
+
+const MapEntry = struct {
+    from: u32,
+    to: u32,
+    len: u32,
+};
 
 pub fn main() !void {
     const in = std.io.getStdIn();
@@ -27,12 +33,51 @@ pub fn main() !void {
     var in_r = in_buf.reader();
     var line_buf: [1024]u8 = undefined;
 
-    //var sum: usize = 0;
+    // Initialize allocator for dynamic memory use
+    var gpa = Gpa{};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
 
+    // Initialize mapping lists
+    var maps: [7]List(MapEntry) = undefined;
+    for (&maps) |*map| {
+        map.* = try List(MapEntry).initCapacity(alloc, 64);
+    }
+    defer for (maps) |map| {
+        map.deinit();
+    };
+
+    // Parse seeds
     const first_line = (try in_r.readUntilDelimiterOrEof(&line_buf, '\n')).?;
-    const seeds = try parseList(first_line[6..]);
-    for (seeds) |seed| {
+    const seeds = try parseList(alloc, first_line[6..]);
+    defer seeds.deinit();
+
+    // Parse maps
+    var n: usize = 0;
+    while (try in_r.readUntilDelimiterOrEof(&line_buf, '\n')) |line| {
+        if (std.mem.endsWith(u8, line, "map:")) {
+            n += 1;
+            continue;
+        }
+
+        const list = parseList(alloc, line) catch continue;
+        defer list.deinit();
+        if (list.items.len != 3) {
+            continue;
+        }
+
+        try maps[n - 1].append(.{ .from = list.items[1], .to = list.items[0], .len = list.items[2] });
+    }
+
+    for (seeds.items) |seed| {
         std.debug.print("Seed {}\n", .{seed});
+    }
+
+    for (maps, 1..) |map, i| {
+        std.debug.print("\nMap {}:\n", .{i});
+        for (map.items) |entry| {
+            std.debug.print("\t{any}\n", .{entry});
+        }
     }
 
     return;
