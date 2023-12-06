5ecb936e9968177bd36ea5b27591de88d6d27927
Lauri Gustafsson <me@gustafla.space>
2023-12-06 10:30:30 +0200

    linereader.s: Add integer parsing routine

--- linereader.s
+++ linereader.s
@@ -9,7 +9,41 @@
 line_buffer:
     .zero   1024
 
+    .type   times, %object
+    .size   times, 32
+times:
+    .zero   32
+
+    .type   dists, %object
+    .size   dists, 32
+dists:
+    .zero   32
+
 .text
+
+
+parse_int:
+    mov     x7, 0               // initialize the integer to which we build up
+    mov     x6, 1               // initialize "place"
+    ldr     x5, =line_buffer
+    mov     x4, 10
+parse_int_next:
+    cmp     x0, x5              // Check that current pointer is not under line_buffer's address
+    b.lt    parse_int_ret
+    ldrb    w1, [x0]            // Check that current character is:
+    cmp     w1, '0'             //
+    b.lt    parse_int_ret       // greater than 0x29
+    cmp     w1, ':'             //
+    b.gt    parse_int_ret       // less than 0x3A
+    sub     x1, x1, '0'         // convert to number
+    madd    x7, x1, x6, x7      // total = total + (number * place)
+    mul     x6, x6, x4          // place = place * 10
+    sub     x0, x0, 1           // ptr = ptr - 1
+    b       parse_int_next      // loop back to next
+parse_int_ret:
+    mov     x0, x7
+    ret
+
 
 read_line:
     sub     sp, sp, 32          // create a new stack frame
@@ -46,10 +80,19 @@ read_line_ret:
     add     sp, sp, 32
     ret
 
+
 .globl _start
 _start:
     bl      read_line
-    mov     x2, x0              // use return value as count-parameter
+    mov     x19, x0
+
+    ldr     x7, =line_buffer    // get line_buffer address
+    add     x0, x0, x7          // point to last character in line
+    sub     x0, x0, 2           // back 2 characters (1 for len -1 and 1 for '\n')
+    bl      parse_int           // parse_int
+
+echo:
+    mov     x2, x19             // use line length value as count-parameter
                                 // syscall write(int fd, const void *buf, size_t count)
     mov     x0, 1               // fd := STDOUT_FILENO
     ldr     x1, =line_buffer
